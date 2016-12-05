3d95d0f289c1fba6a05932de985e35b1d5487e66
lukamarijn <lukabrinkmannn@gmail.com>
2016-12-05 13:27:31 +0100

    final

--- src/trio/presentation/controlPanel.java
+++ src/trio/presentation/controlPanel.java
@@ -66,7 +66,6 @@ public controlPanel(netflixView view){
         
         button[i] = new JButton("Overzicht " + (i+1));
         button[i].addActionListener(new controlHandler()); 
-        button[i].setActionCommand(""+ i + 1);
         button[i].setBorderPainted(false);
         button[i].setContentAreaFilled(false);
         button[i].setOpaque(true);
